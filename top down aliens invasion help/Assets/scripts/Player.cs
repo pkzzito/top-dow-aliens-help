fa9dae23f397f808db3a4788a627c2a6b466e734
Pedrokauaalm <126105518+Pedrokauaalm@users.noreply.github.com>
2025-11-22 12:16:16 -0300

    arma e mira

--- top down aliens invasion help/Assets/scripts/Player.cs
+++ top down aliens invasion help/Assets/scripts/Player.cs
@@ -12,30 +12,38 @@ public class Player : Personagem
     void Start()
     {
         spriteRenderer = GetComponent <SpriteRenderer>();
-        animator = GetComponent <Animato>(;)
+        animator = GetComponent<Animator>();
     }
 
    
     void Update()
     {
+        andando = false;
+        
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position += new Vector3 (0, getVelocidade() * Time.deltaTime, 0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.position -= new Vector3 (0, getVelocidade() * Time.deltaTime, 0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3 (getVelocidade() * Time.deltaTime, 0, 0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3 (getVelocidade() * Time.deltaTime, 0, 0);
+            andando = true;
         }
+        
+        animator.SetBool("Andando", andando);
     }
 }
---
+++ top down aliens invasion help/Assets/scripts/arma.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class arma : MonoBehaviour
+{
+
+    public Transform saidaTiro;
+
+    public GameObject bala;
+
+    public float intervaloDeDisparo = 0.2f;
+    
+    private float tempoDeDisparo = 0;
+
+    private Camera camera;
+    public GameObject cursor;
+    
+    private SpriteRenderer spriteRenderer;
+    
+    void Start()
+    {
+        camera = Camera.main;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+   
+    void Update()
+    {
+        if (gameObject.transform.rotation.eulerAngles.z > -90 && gameObject.transform.rotation.eulerAngles.z < 90)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (gameObject.transform.rotation.eulerAngles.z > 90 && gameObject.transform.rotation.eulerAngles.z < 270)
+        {
+            transform.localScale = new Vector3(1, -1, 1);
+        }
+        
+        // obtenha a camera ao objeto. Precisamos disso para fazer o calculo correto.
+        float camDis = camera.transform.position.y - transform.position.y;
+        
+        // obtea a posição do mouse no espaço mundial. usando amDis para o eixo z.
+        Vector3 mouse = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+        
+        float AngleRad = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+        
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+        // Debug.Log("Angulo: "+angle)
+
+        cursor.transform.position = new Vector3(mouse.x, mouse.y, cursor.transform.position.z);
+
+        Debug.DrawLine(transform.position, mouse, Color.red);
+
+
+        if (tempoDeDisparo <= 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Bala Disparada");
+            
+            GameObject b = Instantiate(bala, saidaTiro.position, saidaTiro.rotation) as GameObject;
+            
+            tempoDeDisparo = intervaloDeDisparo;
+        }
+
+        if (tempoDeDisparo > 0)
+        {
+            tempoDeDisparo -= Time.deltaTime;
+        }
+    }
+}
